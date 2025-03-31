1874689887a69794513827dfdb481a3765f1886e
Daniel Laureano <daniellaureanoluz@gmail.com>
2025-03-31 17:33:21 -0300

    Projeto Aula 1 Imersão Dev Alura

---
+++ conversor-imersao-dev/README.md
@@ -0,0 +1,6 @@
+# Conversor - Imersão Dev Alura
+
+A Pen created on CodePen.
+
+Original URL: [https://codepen.io/daniel-laureano-the-bashful/pen/OPJazbg](https://codepen.io/daniel-laureano-the-bashful/pen/OPJazbg).
+
---
+++ conversor-imersao-dev/dist/script.js
@@ -0,0 +1,17 @@
+function conversorDolar() {
+valor = prompt("Digite um valor em reais: ");
+
+dolar = 5.71;
+conversao = valor * dolar;
+alert("U$" + conversao)
+alert("Que a força esteja com você!")
+}
+
+function conversorEuro() {
+valor = prompt("Digite um valor em reais: ");
+
+euro = 6.19;
+conversao = valor * euro;
+alert(conversao + "€")
+alert("Que a força esteja com você!")
+}
